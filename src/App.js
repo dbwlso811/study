9d85899e25c49d4ba7e34a15855c207c32ac3e0d
kimyoojin <dbwls_0811@naver.com>
2021-06-21 16:43:47 +0900

    [React-study] [   1. class 대신 className ]

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
+import './App.css';
 
 function App() {
     const name = '리액트';
-    const style = {
-        // background-color -> backgroundColor (카멜 표기법)
-        backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '48px',
-        fontWeight: 'bold',
-        padding: 16 // 단위생량하면 px로 지정됨
-    }
-    return <div style={style}>{name}</div>;
+    return <div className='react'>{name}</div>;
 }
 
 export default App;
