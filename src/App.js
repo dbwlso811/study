0c29f375806530e700fc0d54bff7e74e89d4eef6
kimyoojin <dbwls_0811@naver.com>
2021-06-21 16:22:08 +0900

    [React-study] [   1. and 연산자 ]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
 function App() {
-    const name = '리액트';
+    const name = '리액터';
     return (
         <div>
-            {name === '리액트' ? (
-                <h1>리액트 입니다.</h1>
-            ) : (
-                <h2>리액트가 아닙니다.</h2>
-            )}
+            {name === '리액트' &&
+                <h1>리액트 입니다.</h1>}
         </div>
     );
 }
