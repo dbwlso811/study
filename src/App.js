e976aca9139400b4ad0fdbf115ffd9ae912a3308
kimyoojin <dbwls_0811@naver.com>
2021-06-21 16:36:27 +0900

    [React-study] [   1. 카멜표기법 ]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function App() {
-    const name = undefined;
-    return <div>{name || '리액트'}</div>;
+    const name = '리액트';
+    const style = {
+        // background-color -> backgroundColor (카멜 표기법)
+        backgroundColor: 'black',
+        color: 'aqua',
+        fontSize: '48px',
+        fontWeight: 'bold',
+        padding: 16 // 단위생량하면 px로 지정됨
+    }
+    return <div style={style}>{name}</div>;
 }
 
 export default App;
