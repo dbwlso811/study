2ff5ea9fb68efd929ee487791b5ab15a65bc283c
kimyoojin <dbwls_0811@naver.com>
2021-06-21 15:07:31 +0900

    [React-study] [   1. 자바스크립트 표현 ]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
 function App() {
+    const name = '리액트';
     return (
-        <Fragment>
-            <h1>리액트 안녕!</h1>
+        <>
+            <h1>{name} 안녕!</h1>
             <h2>잘 작동하니?</h2>
-        </Fragment>
+        </>
     );
 }
 
