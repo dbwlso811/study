96afd7905bcf41acfc6b9c60fe9861504a301b59
kimyoojin <dbwls_0811@naver.com>
2021-06-21 16:57:49 +0900

    [React-study] [   1. 주석 ]

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
     const name = '리액트';
     return (
         <>
+            {/*주석은 이렇게 작성합니다.*/}
             <div className='react'>{name}</div>
-            <input/>
         </>
+        // 하지만 이런 주석이나
+        /* 이런 주석은 페이지에 그대로 나타나게 됩니다. */
     );
 }
 
