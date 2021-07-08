ff473fdcf09f8c481fad17b33411d71cec05b652
kimyoojin <dbwls_0811@naver.com>
2021-07-08 19:41:09 +0900

    [text] [   1. .. ]

--- src/Counter.js
+++ src/Counter.js
@@ -1,31 +1,22 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class Counter extends Component {
-    constructor(props) {
-        super(props);
-        // state의 초깃값 설정하기
-        this.state = {
-            number: 0
-        };
-    }
+const Counter = () => {
+    const [ number,setNumber ] = useState(0);
+    const onClick = () => {
+      alert(number + 1);
+    };
+    return (
+        <div>
+            <h1>{number}</h1>
+            <button
+                // onClick을 통해 버튼이 클릭되었을때 호출할 함수를 지정
+                onClick={onClick}>
+                +1
+            </button>
+        </div>
+    );
 
-    render() {
-        const {number} = this.state; // state를 조회 할때는 this.state로 조회함
-        return (
-            <div>
-                <h1>{number}</h1>
-                <button
-                    // onClick을 통해 버튼이 클릭되었을때 호출할 함수를 지정
-                    onClick={() => {
-                        // this.setState를 사용하여 state에 새로운 값을 넣을 수 있음
-                        this.setState({number: number + 1});
-                    }}
-                >
-                    +1
-                </button>
-            </div>
-        );
-    }
-}
+
+};
 
 export default Counter;
